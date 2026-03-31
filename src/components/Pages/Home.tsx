a0730670ba9e1d7747ff6b8531a30d20fbc3d2f4
Rebeljoka <habib.u99@googlemail.com>
2026-03-31 14:11:21 +0100

    Update: created divs to allow content to be displayed on new line

--- src/components/Pages/Home.tsx
+++ src/components/Pages/Home.tsx
@@ -3,18 +3,23 @@ import TextType from "../TextType";
 
 export default function Home() {
 	return (
-		<div className="w-full min-h-screen flex mt-100 justify-center text-white">
-			<h1 className="text-9xl font-black">Habib Ullah</h1>
-            <h1 className="text-9xl font-black px-8">|</h1>
-            <TextType className="text-9xl font-black"
-                text={["Software Developer", "Systems Integrator", "Graphics Designer", "Tech Enthusiast"]}
-                typingSpeed={75}
-                pauseDuration={1500}
-                showCursor
-                cursorCharacter="|"
-                deletingSpeed={50}
-                cursorBlinkDuration={0.5}
-            />
-		</div>
+		<section className="w-full min-h-screen mt-128 flex justify-center text-white text-center">
+            <div className="Name">
+                <div>
+                    <h1 className="text-9xl font-black">Habib Ullah</h1>
+                </div>
+                <div>
+                    <TextType className="text-9xl font-black"
+                        text={["Software Developer", "Systems Integrator", "Graphics Designer", "Tech Enthusiast", "Video Editor", "Content Creator"]}
+                        typingSpeed={75}
+                        pauseDuration={1500}
+                        showCursor
+                        cursorCharacter="|"
+                        deletingSpeed={50}
+                        cursorBlinkDuration={0.5}
+                    />
+                </div>
+            </div>
+		</section>
 	);
     }
